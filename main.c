9235da7e6e944a725f80d2c5d9c696e64aafb0c7
DalilaPatimin <dalila543@gmail.com>
2020-10-30 16:06:09 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+ 
+int main()
+{   
+    int loop, number;
+    int prime = 1 ;
+
+  while(printf("Key number :") && scanf("%d",&number)){
+  if(number<0)break;
+  if(number == 1 || number == 0 ){
+    printf ("%d Not prime number !!! \n",number);
+    continue;
+  }
+ for(loop = 2 ; loop < number ; loop++){
+   if((number % loop) == 0){
+     prime = 0;
+   }
+ }
+if (prime == 1){
+  printf("%d prime number.\n", number );
+  for (int i = 1 ; i <= 12 ; i++){
+    printf ("%d * %d = %d \n",number,i,number*i);
+  }
+}
+else if(prime == 0 || number == 1)
+  printf ("%d Not prime number !!! \n",number);
+prime = 1;
+  }  
+    return 0;
+}
